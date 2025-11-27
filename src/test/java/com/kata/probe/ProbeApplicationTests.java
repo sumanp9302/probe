6b0089d19d395ffd0b32204591a51ba20b9607b5
sumanp <sumanprofile9302@gmail.com>
2025-11-27 16:28:14 +0530

    chore: bootstrap Spring Boot 3 + Java 21 skeleton (pom + app)

---
+++ src/test/java/com/kata/probe/ProbeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kata.probe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProbeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
