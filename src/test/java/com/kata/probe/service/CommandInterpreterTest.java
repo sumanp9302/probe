5310a56ba2b54c5c9d210639a6308640bbb3f080
sumanp <sumanprofile9302@gmail.com>
2025-11-28 11:56:25 +0530

    Package restructure

--- src/test/java/com/kata/probe/service/CommandInterpreterTest.java
+++ src/test/java/com/kata/probe/service/CommandInterpreterTest.java
@@ -1,11 +1,9 @@
-package com.kata.probe;
+package com.kata.probe.service;
 
 import com.kata.probe.domain.*;
-import com.kata.probe.service.CommandInterpreter;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
-import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
